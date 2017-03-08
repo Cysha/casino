f8253010563e0b723e21ac85c87d035dfaf661a9
Dan Aldridge <xLink@cybershade.org>
2017-03-08 00:44:25 +0000

    added notRegistered method to GameException

--- src/Exceptions/GameException.php
+++ src/Exceptions/GameException.php
@@ -43,6 +43,21 @@ class GameException extends Exception
      *
      * @return static
      */
+    public static function notRegistered(Client $player, Game $game, $message = null)
+    {
+        $defaultMessage = sprintf('%s is not registered to game: "%s"', $player, $game);
+        $message = is_null($message) ? $defaultMessage : $message;
+
+        return new static($message);
+    }
+
+    /**
+     * @param Client $player
+     * @param Game   $game
+     * @param string $message
+     *
+     * @return static
+     */
     public static function insufficientFunds(Client $player, Game $game, $message = null)
     {
         $defaultMessage = sprintf('%s doesnt have sufficient funds to register for game: "%s"', $player, $game);
